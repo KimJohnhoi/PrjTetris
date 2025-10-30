9bc48713dfec069fcb96395c8f5f77089ac6fbbd
KimJohnhoi <johnhoi99@gmail.com>
2025-10-30 17:06:07 +0900

    1030 검증중

--- tetris.js
+++ tetris.js
@@ -41,17 +41,14 @@ board = {
         }
         // 드롭되는 랜덤블록 보드에 맵핑
         for(let i = 0; i < piece.shape.length; i++){
-            tempX = i + 2;
+            tempY = piece.position.y + i;
             for(let j = 0; j < piece.shape.length; j++){
-                tempY = j + piece.position.x;
-                console.log(`Y값: ${tempY}`);
-                temp = tempX + "" + tempY;
+                tempX = j + piece.position.x;
+                temp = tempY + "" + tempX;
                 temp = temp * 1;
                 cell = document.getElementsByTagName('span')[temp];
 
-                if(piece.shape[i][j] > 0) { 
-                    cell.className = "cell-" + (no+1);
-                }
+                if(piece.shape[i][j] > 0 && cell) cell.className = "cell-" + (no+1);
             }
         }
     } // End of drawCell()
@@ -59,8 +56,8 @@ board = {
 
 piece = {
     position: { 
-        x: 0, 
-        y: 0 
+        x: 0,
+        y: 0
     },
     shape: [],
     roate: () => { // 블럭 회전
@@ -87,17 +84,41 @@ piece = {
         let newY = piece.position.y + 1;
     }, // End of stepDown()
     stepSide: x => { // 블럭 좌우 이동
+        let xx = piece.position.x;
         if(x === 'left'){
-            piece.position.x = piece.position.x - 1;
+            if(xx === -2) return;
+            xx--;
         } else if(x === 'right'){
-            piece.position.x = piece.position.x + 1;
+            if(xx === 8) return;
+            xx++;
         }
-        board.drawCell();
+        if(piece.verify({x:xx})){
+            piece.position.x = xx;
+            console.log("Verified :: " + xx)
+            board.drawCell();
+        }else console.log("Not verified :: " + xx)
     }, // End of stepSide()
+    verify: (p) => {
+        let x, y, t;
+        if(p == undefined) return;
+        if(p.x == undefined) p.x = piece.position.x;
+        if(p.y == undefined) p.y = piece.position.y;
+
+        for(y = 0 ; y < 4 ; y++){
+            for(x = 0 ; x < 4 ; x++){
+                console.log(`(x,y)/(p.x,p.y):(${x},${y}) / (${p.x},${p.y})`)
+                //t = board.body[p.x+x+2][p.y+y+2] * piece.shape[x][y]; // 큰 귀요미꺼
+                t = board.body[p.x+x+1][p.y+y+1] * piece.shape[x][y]; // (x+1)(y+1) => 좌우에 따라 달라져야하는것 아닌가?? 
+                console.log(`t: ${t}`);
+                if(t === 0) return true; // 수정 
+            }
+        }
+        return false;
+    },
     setNew:() => {
         let no, blocks = [
-            [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]], // "I" - 0 
-            [[1,0,0,0],[1,0,0,0],[1,0,0,0],[1,0,0,0]], // "O" - 1
+            [[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]], // "I" - 0 
+            [[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]], // "O" - 1
             [[0,0,0,0],[1,1,1,0],[0,1,0,0],[0,0,0,0]], // "T" - 2
             [[0,0,0,0],[0,0,1,0],[0,0,1,0],[0,1,1,0]], // "J" - 3
             [[0,0,0,0],[0,1,0,0],[0,1,0,0],[0,1,1,0]], // "L" - 4
@@ -107,6 +128,7 @@ piece = {
 
         no = Math.floor(Math.random() * blocks.length); // 랜덤으로 블럭 뽑기
         piece.position.x = 3;
+        piece.position.y = 0;
         piece.shape = blocks[no];
 
         return no;
@@ -127,7 +149,9 @@ motion = {
 init = () => {
     //motion.start();
     board.container = document.getElementsByClassName('board')[0];
-    
+    board.init();
+    board.getCell();
+
     let el1, els;
     els = document.getElementsByClassName('wrap')[0].getElementsByClassName('button')[0];
     el1 = els.children[1]; 
@@ -137,8 +161,10 @@ init = () => {
         board.drawCell();
     }
 
-    window.addEventListener('keyup', e => { // 키보드 좌우 이벤트 등록 Error
+    let f = e => { // 키보드 좌우 이벤트 등록
         if(e.key === 'ArrowLeft') piece.stepSide('left');
         else if(e.key === 'ArrowRight') piece.stepSide('right');
-    });
+    };
+    window.setTimeout(()=>window.onkeyup = f,1000);
+    
 } // End of init
